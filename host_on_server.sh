2a71f05bf5e49a62ea654a337de5338279ba9334
Ziyang Huang <35769263+sheepy928@users.noreply.github.com>
2024-04-29 19:47:10 -0400

    Update server port in host_on_server.sh script

--- host_on_server.sh
+++ host_on_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    streamlit run Welcome.py
+    streamlit run Welcome.py --server.port 8501
     echo "Streamlit has stopped. Attempting to restart in 5 seconds..."
     sleep 5
     # Replace with your actual notification command
